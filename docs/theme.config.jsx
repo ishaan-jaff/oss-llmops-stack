9cf3c2a4702774898c55bcf5efe4be90a58e6536
Marc Klingen <git@marcklingen.com>
2025-02-09 04:23:47 +0100

    feat: update seo, favicon, footer, other theme config

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,36 @@
+import { useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 export default {
   logo: <span>OSS LLMOps Stack</span>,
   project: {
     link: "https://github.com/langfuse/oss-llmops-stack",
   },
+  docsRepositoryBase:
+    "https://github.com/langfuse/oss-llmops-stack/tree/main/docs",
+
+  head() {
+    const { url } = useRouter();
+    const { frontMatter } = useConfig();
+
+    const title = frontMatter.title
+      ? `${frontMatter.title} - The OSS LLMOps Stack`
+      : "The OSS LLMOps Stack";
+    const description = frontMatter.description;
+
+    return (
+      <>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </>
+    );
+  },
+  gitTimestamp: null,
+  footer: {
+    content: "The OSS LLMOps Stack",
+  },
+  faviconGlyph: "⚡️",
   // ... other theme options
 };
