d65b07c6b28e414aaa907340b8ff77794e32f9ed
Marc Klingen <git@marcklingen.com>
2025-02-09 03:01:18 +0100

    feat: docs via nextra

---
+++ docs/pages/_meta.js
@@ -0,0 +1,13 @@
+export default {
+    index: {
+        title: 'About',
+        type: 'page',
+        theme: {
+            typesetting: 'article'
+        }
+    },
+    "get-started": {
+        title: 'Get Started',
+        type: 'page'
+    },
+}
---
+++ docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>OSS LLMops Stack</span>,
+  project: {
+    link: "https://github.com/langfuse/oss-llmops-stack",
+  },
+  // ... other theme options
+};
