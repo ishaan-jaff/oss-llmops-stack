503cb63c6102abb946aaf87004b385f07830ae5a
Marc Klingen <git@marcklingen.com>
2025-02-14 01:37:09 +0100

    feat: add posthog (#4)

--- docs/pages/_app.jsx
+++ docs/pages/_app.jsx
@@ -1,6 +1,45 @@
 import "../src/style.css";
 import "../src/overrides.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+// Remove the top-level PostHog initialization since we'll do it in the useEffect hook
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize PostHog
+    if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: "https://ph.oss-llmops-stack.com",
+        ui_host: "https://eu.posthog.com",
+        // Enable debug mode in development
+        loaded: (posthog) => {
+          if (process.env.NODE_ENV === "development") posthog.debug();
+        },
+        autocapture: false,
+        enable_heatmaps: false,
+        disable_surveys: true,
+      });
+    }
+
+    // Track page views
+    const handleRouteChange = () => {
+      posthog.capture("$pageview");
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  );
 }
